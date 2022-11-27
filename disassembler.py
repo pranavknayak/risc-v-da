2c9580f9b282576cf01fd62f2cf81c98a40eb0f0
Pranav K Nayak <es20btech11035@iith.ac.in>
2022-11-27 11:56:06 +0530

    Converts hexadecimal input to binary

--- disassembler.py
+++ disassembler.py
@@ -1,1 +1,13 @@
-print("Hello World")
+input_file = open("Input.txt", "r")
+output_file = open("Output.s", "w")
+
+hex_instructions = input_file.read()
+hex_instructions = hex_instructions.splitlines()
+for hex in hex_instructions:
+    integer = int(hex, 16)
+    binary = format(integer, '0>32b')
+    output_file.write(binary)
+    output_file.write('\n')
+
+input_file.close()
+output_file.close()
