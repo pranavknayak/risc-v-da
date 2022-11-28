e975a235d5f565a10c03f90ce0727577e59eb15a
Pranav K Nayak <es20btech11035@iith.ac.in>
2022-11-28 17:30:12 +0530

    finished support for R-Type instructions

--- disassembler.py
+++ disassembler.py
@@ -9,11 +9,12 @@ for hex in hex_instructions:
     binary = format(integer, '0>32b')
     binary_list.append(binary)
 
-instruction_list = []
+instruction_list = [""]*len(hex_instructions)
+instruction_count = 0
 for binary in binary_list:
     instruction = ""
     opcode = binary[-7:]
-    print(opcode)
+    # R-type instruction handler
     if opcode == '0110011':
         rd = binary[-12:-7]
         funct3 = binary[-15:-12]
@@ -23,17 +24,33 @@ for binary in binary_list:
 
         if funct7 == '0000000':
             if funct3 == '000':
-                instruction.append("add ")
+                instruction += "add "
             elif funct3 == '100':
-                instruction.append("xor ")
+                instruction += "xor "
             elif funct3 == '110':
-                instruction.append("or ")
+                instruction += "or "
             elif funct3 == '111':
-                instruction.append("and ")
+                instruction += "and "
             elif funct3 == '001':
-                instruction.append("sll ")
+                instruction += "sll "
             elif funct3 == '101':
-                instruction.append("srl ")
+                instruction += "srl "
+        elif funct7 == '0100000':
+            if funct3 == '000':
+                instruction += "sub "
+            elif funct3 == '101':
+                instruction += "sra "
+        rd = str(int(rd, 2))
+        rs1 = str(int(rs1, 2))
+        rs2 = str(int(rs2, 2))
 
+        instruction = instruction + "x" + rd + ", x" + rs1 + ", x" + rs2
+
+    #elif opcode == ''
+
+    instruction_list[instruction_count] += instruction
+    output_file.write(instruction_list[instruction_count])
+    output_file.write("\n")
+    instruction_count += 1
 input_file.close()
 output_file.close()
