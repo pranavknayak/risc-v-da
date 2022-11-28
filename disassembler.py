b755e8bac6ad1284b163e98b4352811a7e069c63
Pranav K Nayak <es20btech11035@iith.ac.in>
2022-11-28 18:06:55 +0530

    finished support for non-load I-type instructions

--- disassembler.py
+++ disassembler.py
@@ -46,7 +46,33 @@ for binary in binary_list:
 
         instruction = instruction + "x" + rd + ", x" + rs1 + ", x" + rs2
 
-    #elif opcode == ''
+    elif opcode == '0010011':
+        rd = binary[-12:-7]
+        funct3 = binary[-15:-12]
+        rs1 = binary[-20:-15]
+        immediate = binary[-32:-20]
+        if funct3 == '000':
+            instruction += "addi "
+        elif funct3 == '100':
+            instruction += "xori "
+        elif funct3 == '110':
+            instruction += "ori "
+        elif funct3 == '111':
+            instruction += "andi "
+        elif funct3 == '001':
+            instruction += "slli "
+        elif funct3 == '101':
+            # srli or srai
+            if immediate[:6] == '000000':
+                instruction += 'srli '
+            elif immediate[:6] == '010000':
+                instruction += 'srai '
+
+        rd = str(int(rd, 2))
+        rs1 = str(int(rs1, 2))
+        immediate = str(int(immediate[6:], 2))
+
+        instruction += "x" + rd + ", x" + rs1 + ", " + immediate
 
     instruction_list[instruction_count] += instruction
     output_file.write(instruction_list[instruction_count])
